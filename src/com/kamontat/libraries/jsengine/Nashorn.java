c3be205faf5eff2785afc7c75876ff57fb9120f9
kamontat <kamontat_c@hotmail.com>
2016-05-10 00:00:01 +0700

    finish javaScript engine

--- src/com/kamontat/libraries/jsengine/Nashorn.java
+++ src/com/kamontat/libraries/jsengine/Nashorn.java
@@ -1,8 +1,19 @@
 package com.kamontat.libraries.jsengine;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 /**
  * @author kamontat
  * @since 9/5/59 - 23:51
  */
 public class Nashorn {
+	public static void main(String[] args) throws ScriptException {
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByName("JavaScript");
+
+		System.out.println(engine.getClass().getName());
+		System.out.println("Result: " + engine.eval("function f() { return 1; } f() + 1;"));
+	}
 }
