64b6e9ac8cc1051fdf8ee86a857919df1f62b930
kamontat <kamontat_c@hotmail.com>
2016-05-10 01:06:39 +0700

    add cal how many time than sort slower than parallel

--- src/com/kamontat/libraries/parallelarray/ParallelArrays.java
+++ src/com/kamontat/libraries/parallelarray/ParallelArrays.java
@@ -12,21 +12,33 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ParallelArrays {
 	public static void main(String[] args) {
 		StopWatch watch = new StopWatch();
-		long[] arrayOfLong = new long[20000];
-		Arrays.parallelSetAll(arrayOfLong, value -> ThreadLocalRandom.current().nextInt(1000000));
+		long[] arrayOfLong = new long[100000000];
+		Arrays.parallelSetAll(arrayOfLong, value -> ThreadLocalRandom.current().nextInt(1000000000));
+
 		Arrays.stream(arrayOfLong).limit(10).forEach(i -> System.out.print(i + " "));
 		System.out.println();
 
 		watch.start();
 		Arrays.sort(arrayOfLong);
 		watch.stop();
+		double arrayTime = watch.getElapsed();
 		watch.printTime();
 		watch.reset();
 
 		watch.start();
 		Arrays.parallelSort(arrayOfLong);
 		watch.stop();
+		double parallelTime = watch.getElapsed();
 		watch.printTime();
 		watch.reset();
+
+		System.out.println("different time is ");
+		if (arrayTime > parallelTime) {
+			System.out.printf("array more than parallel: %.1f times", (arrayTime / parallelTime));
+		} else if (arrayTime < parallelTime) {
+			System.out.printf("array more than parallel: %.1f times", (parallelTime / arrayTime));
+		} else {
+			System.out.print("equals");
+		}
 	}
 }
